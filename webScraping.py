1de70f03ffb0f1447cf46dae4a41feee18f0d032
Leoleoncio <leone.hollanda@hotmail.com>
2022-09-07 15:34:14 -0300

    sim

--- webScraping.py
+++ webScraping.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.token import OP
 import site
 import pandas as pd
 import json
@@ -9,8 +10,12 @@ from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-i = 0
+
+options = Options()
+#options.add_argument(' --headless')
+options.add_argument('window-size=1980,1080')
 url = 'https://www.vipleiloes.com.br/'
 
 headers = {
@@ -21,9 +26,13 @@ site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 respostas = soup.find_all("div", class_='card crd-main d-flex')
 servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico)
+
+navegador = webdriver.Chrome(service=servico, options=options)
 navegador.get(url)
-sleep(5)
+# navegador.execute_script('document.body.style.zoom="25%"')
+
+# navegador.execute_script('document.body.style.zoom="25%"')
+sleep(10)
 
 #kmRodada = soup.find("div", class_='crd-linefour').get_text()
 tabela = {'nome': [], 'marca': [], 'preco': []}
@@ -40,10 +49,31 @@ for i, d in enumerate(respostas):
         tabela['preco'].append(preco)
         navegador.find_element(
             By.XPATH, f'//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[2]/div/div[{1+i}]').click()
-        sleep(5)
+        sleep(0.5)
         navegador.find_element(
             By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/header/nav/div/a/img').click()
-        sleep(5)
+        sleep(0.5)
+        print(f'conseguiu pegar {i+1}')
+        if i > 6:
+            navegador.find_element(
+                By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+            if i > 10:
+                navegador.find_element(
+                    By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+                if i > 14:
+                    navegador.find_element(
+                        By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+                    if i > 18:
+                        navegador.find_element(
+                            By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+                        if i > 22:
+                            navegador.find_element(
+                                By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+                            if i > 26:
+                                navegador.find_element(
+                                    By.XPATH, '//*[@id="sidebar-menu-container"]/div[2]/div[4]/div/div/div[3]/a').click()
+
+
 # print(tabela)
 tab = pd.DataFrame(tabela)
 print(tab)
