916c4b47e984575326546f9448db66da9e29a9a5
Leoleoncio <leone.hollanda@hotmail.com>
2022-10-13 19:01:25 -0300

    blobTeste

--- blobTest.py
+++ blobTest.py
@@ -5,8 +5,12 @@ import os
 url = 'https://www.vipleiloes.com.br/Veiculos/DetalharVeiculo/ca4db281-aeb2-43aa-b933-2b8ace491d65'
 site = re.get(url)
 soup = BeautifulSoup(site.content, 'html.parser')
-fotos = soup.find('img',class_="dtp-imgactive")['src']
-with open(fotos.replace(' ','-').replace('/',','), 'wb')as f:
+fotos = soup.find('img')['src']
+nome = soup.find('span',class_='dtp-fullnm').get_text().replace('\n',' ')
+print(nome)
+print(fotos)
+
+with open(nome,'wb')as f:
     img=re.get(fotos)
     f.write(img.content)
 print(fotos)
