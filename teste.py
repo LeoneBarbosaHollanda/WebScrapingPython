7c6977e4f238c164a8751d72051eebb52d0aa277
Leoleoncio <leone.hollanda@hotmail.com>
2022-10-20 18:20:13 -0300

    he

--- teste.py
+++ teste.py
@@ -1,4 +1,19 @@
-teste = {'test1': [1, 2, 3, 4, 5, 6, 7, ],
-         'test2': [1, 57, 5, 3, 12, 3234, 12]}
-var = 1
-print(teste['test' + f'{var}'])
+from pathlib import Path
+from re import X
+import requests
+from bs4 import BeautifulSoup
+import os
+import mysql.connector
+pastateste = 'testes'
+x='imagem'
+localizaçaoteste = Path().absolute()/pastateste
+#foto = Path().absolute()/pastateste/'aaaaa.txt'
+#localizaçaoteste.mkdir()
+"""foto.touch()"""
+with pastateste.open('a+')as f:
+    with open(x,'wb')as d:    
+        img = requests.get('https://armazviplprd.blob.core.windows.net/controle/galeria/padrao/fe5055c3-8fe2-4a3c-9875-a54f34f2c255.jpg')
+        d.write(img.content)
+
+
+print("a localizaçao é esse: ",localizaçaoteste)
